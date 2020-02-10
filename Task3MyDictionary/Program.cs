d9fff4a5929a14fb49ab311fc77f2f358ace7745
DyrElena <lena.d.job@gmail.com>
2020-02-10 22:56:07 +0200

    u

--- Task3MyDictionary/Program.cs
+++ Task3MyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Task3MyDictionary
@@ -7,19 +8,31 @@ namespace Task3MyDictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> test = new Dictionary<int, string>();
-            
             MyDictionary<int, string> test1 = new MyDictionary<int, string>();
-            test1.Add(1, "a");
-            test1.Add(2, "a");
-            for (int i = 0; i < test1.MyLength; i++)
+            test1.Add(1, "one");
+            test1.Add(2, "two");
+            test1.Add(3, "three");
+
+            foreach (var x in test1)
             {
-                Console.WriteLine(test1[i]);
+                Console.WriteLine(x);
             }
+
+
+            Console.WriteLine($"Number of items: {test1.MyLength}\n");
+
+            do
+            {
+                Console.Write("Enter the key: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"The value for key {key} is: {test1[key]}");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+           // Console.ReadLine();
         }
     }
 
-    class MyDictionary<TKey, TValue>
+    class MyDictionary<TKey, TValue> : IEnumerable
     {
         public List<KeyValuePair<TKey, TValue>> myDictionaryElems;
         public int MyLength { get { return myDictionaryElems.Count; } } //общее количество элементов
@@ -32,9 +45,9 @@ namespace Task3MyDictionary
         //добавление элемента
         public void Add(TKey key, TValue value)
         {
-            if (myDictionaryElems.Exists(x => x.Key.Equals(key)))
+            if (KeyExist(key))
             {
-                throw new Exception();
+                throw new Exception("Error 1");
             }
             else
             {
@@ -42,10 +55,35 @@ namespace Task3MyDictionary
             }
         }
 
-        //индексатор
-        public List<KeyValuePair<TKey, TValue>> this [TKey key]
+        
+        private bool KeyExist(TKey key)
         {
-            get { return myDictionaryElems[key]; }
+            return myDictionaryElems.Exists(x => x.Key.Equals(key));
         }
+
+        public TValue GetValue(TKey key)
+        {
+            if(KeyExist(key))
+            {
+                return myDictionaryElems.Find(x => x.Key.Equals(key)).Value;
+            }
+            else
+            {
+                throw new Exception("Error2");
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {   
+            return myDictionaryElems.GetEnumerator();
+        }
+
+        //индексатор
+        public TValue this [TKey key]
+        {
+            get { return GetValue(key); }
+        }
+
+        
     }
 }
